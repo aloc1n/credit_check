bdbba841905832ce86f95db016dbb72efbb957d0
Nicola Kim <nicola.copsey@gmail.com>
2020-03-30 14:26:06 -0700

    attributes tests working for credit card

--- test/credit_card.rb
+++ test/credit_card.rb
@@ -11,4 +11,14 @@ class CreditCardTest < Minitest::Test
     assert_instance_of CreditCard, credit_card
   end
 
+  def test_it_has_a_card_number
+    credit_card = CreditCard.new("5541808923795240", 15000)
+    assert_equal "5541808923795240", credit_card.card_number
+  end
+
+  def test_it_has_a_limit
+    credit_card = CreditCard.new("5541808923795240", 15000)
+    assert_equal 15000, credit_card.limit
+  end
+
 end
