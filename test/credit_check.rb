03ad391974c01dddd0a0f06e7b57897e77cf688f
Nicola Kim <nicola.copsey@gmail.com>
2020-03-30 14:09:21 -0700

    Iteration 1 - The Luhn Algorithm with test

--- test/credit_check.rb
+++ test/credit_check.rb
@@ -5,7 +5,7 @@ require './lib/credit_check'
 class CreditCheckTest < Minitest::Test
 
   def test_it_exists
-    creditcheck = CreditCheck.new
+    creditcheck = CreditCheck.new("5541808923795240")
 
     assert_instance_of CreditCheck, creditcheck
   end
